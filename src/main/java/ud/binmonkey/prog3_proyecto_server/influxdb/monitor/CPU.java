d29bfc162c25cf930db1cedfaade4042cb7f354c
mikelsr <mikelsolabarrieta@protonmail.com>
2017-03-23 11:08:02 +0100

    Isolated server

---
+++ src/main/java/ud/binmonkey/prog3_proyecto_server/influxdb/monitor/CPU.java
@@ -0,0 +1,30 @@
+package ud.binmonkey.prog3_proyecto_server.influxdb.monitor;
+
+import ud.binmonkey.prog3_proyecto_server.common.posix.ShellCommand;
+
+public class CPU {
+
+    public static double getTemperature() {
+        return Double.parseDouble(
+                ShellCommand.executeCommand("/usr/local/p3p/cpu-temperature.sh")
+        );
+    }
+
+    public static double getUsage() {
+        return Double.parseDouble(
+                ShellCommand.executeCommand("/usr/local/p3p/cpu-usage.sh")
+        );
+    }
+
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("CPU usage: " + CPU.getUsage());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
--- src/main/java/ud/ninia/prog3_proyecto/utils/network/HttpMethods.java
+++
@@ -1,8 +0,0 @@
-package ud.ninia.prog3_proyecto.utils.network;
-
-/**
- *  List of supported rest methods
- */
-public enum HttpMethods {
-    GET, POST
-}
